1dddd0cd224835d759b680e2e01b54196b15b1a2
duyguca <duygukoksalca@gmail.com>
2023-11-14 23:20:47 -0800

    fix: add repeater field for stylist

--- page-meet-our-team.php
+++ page-meet-our-team.php
@@ -22,7 +22,7 @@ get_header();
         while (have_posts()) :
             the_post();
 
-            $hero_image = get_field('hero_image_without_text');
+            $hero_image = get_field('hero_image');
             if ($hero_image) : ?>
                 <div class="hero-image-wrapper">
                     <img src="<?php echo esc_url($hero_image['url']); ?>" alt="<?php echo esc_attr($hero_image['alt']); ?>" />
@@ -37,41 +37,28 @@ get_header();
                         <img src="<?php echo esc_url($about_us_image['url']); ?>" alt="<?php echo esc_attr($about_us_image['alt']); ?>" />
                     </div>
                     <h2><?php echo esc_html(get_field('about_us_title')); ?></h2>
-                     <p><?php echo esc_html(get_field('about_us_text')); ?></p>
+                    <p><?php echo esc_html(get_field('about_us_text')); ?></p>
                 </section>
             <?php endif;
 
-             // Stylist 1 section
-            $stylist_image = get_field('stylist_image');
-            if ($stylist_image): ?>
-                <section class="stylist">
-                    
-                    <h2><?php echo esc_html(get_field('stylist_name')); ?></h2>
-                     <p><?php echo esc_html(get_field('stylist_text')); ?></p>
-                      <?php 
-                            $link = get_field('link');
-                            if( $link ): ?>
-                                <a class="button" href="<?php echo esc_url( $link ); ?>">Book Now</a>
-                            <?php endif; ?>
-                     <div class="stylist-img">
-                        <img src="<?php echo esc_url($stylist_image['url']); ?>" alt="<?php echo esc_attr($stylist_image['alt']); ?>" />
-                    </div>
-                </section>
-                 <section class="stylist">
-                    
-                    <h2><?php echo esc_html(get_field('stylist_name')); ?></h2>
-                     <p><?php echo esc_html(get_field('stylist_text')); ?></p>
-                                <?php 
-                        $link = get_field('link');
-                        if( $link ): ?>
-                            <a class="button" href="<?php echo esc_url( $link ); ?>">Book Now</a>
-                        <?php endif; ?>
-                     <div class="stylist-img">
-                        <img src="<?php echo esc_url($stylist_image['url']); ?>" alt="<?php echo esc_attr($stylist_image['alt']); ?>" />
-                    </div>
-                </section>
-            <?php endif;
+            // Assuming the repeater field name is 'stylist' and it is attached to the current page/post
+            $stylists = get_field('stylist');
 
+            if ($stylists) :
+                foreach ($stylists as $stylist) :
+                    // var_dump($stylist["stylist_image"]["url"]);
+                    ?>
+                    <div class="stylist-container">
+                        <h2><?php echo esc_html($stylist['stylist_name']); ?></h2>
+                        <p><?php echo esc_html($stylist['stylist_text']); ?></p>
+                        <img src="<?php echo esc_url($stylist["stylist_image"]["url"]); ?>" alt="">
+                    </div>
+                <?php
+                endforeach;
+            else :
+                // Handle case where there are no stylists
+                echo 'No stylists found.';
+            endif;
 
         endwhile;
 

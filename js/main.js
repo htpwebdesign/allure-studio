2f8bd86c3bc3bec4d230898ef6ae9fa12f8429ea
ACIT-Andrew <115048179+ACIT-Andrew@users.noreply.github.com>
2023-11-29 14:04:59 -0800

    Implement isotope filter

--- js/main.js
+++ js/main.js
@@ -21,10 +21,36 @@ const swiper = new Swiper(".swiper", {
 
 let gallery = document.getElementById("gallery-list");
 lightGallery(gallery, {
-  download: false,
-  controls: false,
-  counter: false,
-  keyPress: false,
-  enableDrag: false,
-  enableSwipe: false,
+    download: false,
+    controls: false,
+    counter: false,
+    keyPress: false,
+    enableDrag: false,
+    enableSwipe: false,
 });
+
+// Isotope filtering
+let isoButtons = document.querySelector(".isotope-buttons");
+let isoGallery = document.querySelector(".gallery-list");
+let iso = new Isotope(isoGallery, {
+    // options
+    itemSelector: ".isotope-item",
+    layoutMode: "fitRows",
+    percentPosition: true
+});
+
+isoButtons.addEventListener("click", function (event) {
+    // only work with li
+    if (!matchesSelector(event.target, "li")) {
+        return;
+    }
+    let filterValue = event.target.getAttribute('data-filter');
+    console.log(filterValue);
+    iso.arrange({filter: filterValue});
+});
+
+// isoButtons.addEventListener("click", function (e) {
+//     let li = e.target;
+//     let filter = li.dataset.filter;
+//     isoButtons.arrange({ filter: filter });
+// });

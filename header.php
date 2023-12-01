c7b95e97bc03f15a4d7046c2be3e0e1345418a2b
ACIT-Andrew <115048179+ACIT-Andrew@users.noreply.github.com>
2023-11-30 16:50:59 -0800

    Edit cart functionality

--- header.php
+++ header.php
@@ -46,7 +46,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'allure-studio' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><svg role="img" aria-label="primary menu" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m22 16.75c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75zm0-5c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75zm0-5c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75z" fill-rule="nonzero"/></svg></button>
 			<?php
 			wp_nav_menu(
 				array(
@@ -54,6 +54,14 @@
 					'menu_id'        => 'primary-menu',
 				)
 			);
+
 			?>
-		</nav><!-- #site-navigation -->
+			<?php
+			if ( function_exists( 'allure_studio_woocommerce_header_cart' ) ) {
+				if(WC()->cart->get_cart_contents_count() > 0) {
+					allure_studio_woocommerce_header_cart();
+				}
+				allure_studio_woocommerce_header_cart();
+			}
+			?>
 	</header><!-- #masthead -->
